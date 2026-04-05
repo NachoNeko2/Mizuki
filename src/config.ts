2273674a203e5ae3b35a27c42448d88ba7dc9f7d
NachoNeko2 <z196482963@163.com>
2026-04-05 15:59:45 +0800

    Update announcement content to Chinese

--- src/config.ts
+++ src/config.ts
@@ -475,7 +475,7 @@ export const shareConfig: ShareConfig = {
 
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "Welcome to the blog! This is a sample announcement", // 公告内容
+	content: "欢迎来到这里，希望你能找到自己的一番天地", // 公告内容
 	closable: true, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接

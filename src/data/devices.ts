54c5a84043c8ba9f1c1388dfb4c10e8dd50c874b
NachoNeko2 <z196482963@163.com>
2026-05-14 20:12:47 +0800

    Update devices.ts

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,12 +16,12 @@ export type DeviceCategory = Record<string, Device[]> & {
 export const devicesData: DeviceCategory = {
 	OnePlus: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.webp",
-			specs: "Gray / 16G + 1TB",
+			name: "OnePlus 13",
+			image: "/images/device/oneplus13.webp",
+			specs: "Gray / 16G + 512G",
 			description:
-				"Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
+				"Flagship performance, Hasselblad imaging, 100W SuperVOOC.",
+			link: "https://www.oneplus.com/cn/13",
 		},
 	],
 	Router: [

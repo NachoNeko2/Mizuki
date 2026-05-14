51a9efef50e11b908099e404239c8dcdf55f20d2
NachoNeko2 <z196482963@163.com>
2026-05-14 11:00:48 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -249,6 +249,14 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 			"/assets/desktop-banner/2.webp",
 			"/assets/desktop-banner/3.webp",
 			"/assets/desktop-banner/4.webp",
+			"/assets/desktop-banner/5.webp",
+			"/assets/desktop-banner/6.webp",
+			"/assets/desktop-banner/7.webp",
+			"/assets/desktop-banner/8.webp",
+			"/assets/desktop-banner/9.webp",
+			"/assets/desktop-banner/10.webp",
+			"/assets/desktop-banner/11.webp",
+			"/assets/desktop-banner/12.webp",
 		], // 桌面横幅图片
 		mobile: [
 			"/assets/mobile-banner/1.webp",
